7f5b352a98a17a4cbb12a6c7d3df808565913e06
labyrinthgarden <ochoasabogaldavid@gmail.com>
2025-08-29 21:31:43 -0500

    Se inicializa el repositorio

---
+++ src/evaluate.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+from dataset import load_dataset
+
+DATA_DIR = "../data/raw"
+MODEL_DIR = "../models/saved_model"
+
+if __name__ == "__main__":
+    val_ds = load_dataset(DATA_DIR)
+    model = tf.keras.models.load_model(MODEL_DIR)
+
+    loss, acc = model.evaluate(val_ds)
+    print(f"-> Loss: {loss:.4f}, Accuracy: {acc:.4f}")
+
